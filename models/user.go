5dd645bb4ebca67eb882fc4e605254826d38c4bd
jonahgeorge <jonah.george@icloud.com>
2014-04-07 12:43:34 -0700

    Removed extraneous code from models/user.go

--- models/user.go
+++ models/user.go
@@ -1,12 +1,1 @@
 package models
-
-import (
-	"database/sql"
-)
-
-type User struct {
-	Id       int64  `db:"id"`
-	Name     string `db:"name"`
-	Email    string `db:"email"`
-	Password string `db:"password"`
-}
