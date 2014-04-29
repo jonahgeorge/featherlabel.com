3f25ccec6ff19a727dee06e094c8c983b7653486
jonahgeorge <jonah.george@icloud.com>
2014-04-29 10:09:07 -0700

    Updated user model for Username rather than Displaynam

--- controllers/users.go
+++ controllers/users.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 	"time"
 
--- models/user.go
+++ models/user.go
@@ -3,32 +3,26 @@ package models
 import "database/sql"
 
 type User struct {
-	Id          int     `json:"id"`
-	Name        string  `json:"name"`
-	Email       string  `json:"email"`
-	DisplayName *string `json:"display_name"`
-	Timestamp   string  `json:"timestamp"`
-	Password    string  `json:"password"`
+	Id        int     `json:"id"`
+	Name      string  `json:"name"`
+	Email     string  `json:"email"`
+	Username  *string `json:"display_name"`
+	Timestamp string  `json:"timestamp"`
+	Password  string  `json:"password"`
+	Biography *string `json:"bio"`
 }
 
 func (u User) RetrieveAll() ([]User, error) {
-
 	var users []User
-
-	rows, err := db.Query(`
-    SELECT 
-      id, name, email, display_name, timestamp, password 
-    FROM 
-      Users
-    `)
+	query := `SELECT * FROM Users`
+	rows, err := db.Query(query)
 	if err != nil {
 		return users, err
 	}
 
 	for rows.Next() {
 		var user User
-		err := rows.Scan(&user.Id, &user.Name, &user.Email, &user.DisplayName,
-			&user.Timestamp, &user.Password)
+		err := rows.Scan(&user.Id, &user.Name, &user.Email, &user.Username, &user.Timestamp, &user.Password)
 		if err != nil {
 			return users, err
 		}
@@ -38,27 +32,16 @@ func (u User) RetrieveAll() ([]User, error) {
 }
 
 func (u User) RetrieveByName(name string) ([]User, error) {
-
 	var users []User
-
-	rows, err := db.Query(`
-    SELECT 
-      id, name, email, display_name, timestamp 
-    FROM 
-      Users 
-    WHERE 
-      name LIKE %?% 
-    OR 
-      display_name LIKE %?%
-    `, name, name)
+	query := `SELECT * FROM Users WHERE name LIke %?% OR username LIKE %?%`
+	rows, err := db.Query(query, name, name)
 	if err != nil {
 		return users, err
 	}
 
 	for rows.Next() {
 		var user User
-		err := rows.Scan(&user.Id, &user.Name, &user.Email, &user.DisplayName,
-			&user.Timestamp)
+		err := rows.Scan(&user.Id, &user.Name, &user.Email, &user.Username, &user.Timestamp, &user.Password)
 		if err != nil {
 			return users, err
 		}
@@ -68,26 +51,16 @@ func (u User) RetrieveByName(name string) ([]User, error) {
 }
 
 func (u User) Featured() ([]User, error) {
-
 	var users []User
-
-	rows, err := db.Query(`
-    SELECT 
-      id, IF(display_name IS NOT NULL, display_name, name)
-    FROM 
-      Users 
-    ORDER BY 
-      RAND()
-    LIMIT 
-      4
-    `)
+	query := `SELECT * FROM Users ORDER BY RAND() LIMIT 4`
+	rows, err := db.Query(query)
 	if err != nil {
 		return users, err
 	}
 
 	for rows.Next() {
 		var user User
-		err := rows.Scan(&user.Id, &user.Name)
+		err := rows.Scan(&user.Id, &user.Name, &user.Email, &user.Username, &user.Timestamp, &user.Password)
 		if err != nil {
 			return users, err
 		}
@@ -98,33 +71,23 @@ func (u User) Featured() ([]User, error) {
 
 func (u User) RetrieveByEmail(email string) (User, error) {
 	var user User
-
-	row := db.QueryRow("SELECT * FROM Users WHERE email = ?", email)
-	err := row.Scan(&user.Id, &user.Name, &user.Email, &user.DisplayName, &user.Timestamp, &user.Password)
-	if err != nil {
-		return user, err
-	}
-
+	query := `SELECT * FROM Users WHERE email = ?`
+	row := db.QueryRow(query, email)
+	err := row.Scan(&user.Id, &user.Name, &user.Email, &user.Username, &user.Timestamp, &user.Password)
 	return user, err
 }
 
 func (u User) RetrieveById(id string) (User, error) {
 	var user User
-
 	query := `SELECT * FROM Users WHERE id = ?`
-
 	row := db.QueryRow(query, id)
-	err := row.Scan(&user.Id, &user.Name, &user.Email, &user.DisplayName, &user.Timestamp, &user.Password)
+	err := row.Scan(&user.Id, &user.Name, &user.Email, &user.Username, &user.Timestamp, &user.Password)
 	return user, err
 }
 
 func (u User) Create(user User) (sql.Result, error) {
-
-	result, err := db.Exec("INSERT INTO Users (name, display_name, email, password) VALUES (?, ?, ?, ?)", user.Name, user.DisplayName, user.Email, user.Password)
-	if err != nil {
-		return result, err
-	}
-
+	query := `INSERT INTO Users (name, display_name, email, password) VALUES (?, ?, ?, ?)`
+	result, err := db.Exec(query, user.Name, user.Username, user.Email, user.Password)
 	return result, err
 }
 
